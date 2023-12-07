b9136acf363e589ade9ce6ff42aff623ed910d86
Long <nguyhonglong2002@gmail.com>
2023-12-07 10:40:28 +0700

    fix

--- frontend/src/BabyAdmin/cpnBabyAdmin/MenuComponent/MenuComponent.js
+++ frontend/src/BabyAdmin/cpnBabyAdmin/MenuComponent/MenuComponent.js
@@ -135,13 +135,111 @@
 
 // export default MenuComponent;
 
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+
+// function DrinkComponent() {
+//   const [drinks, setDrinks] = useState([]);
+//   const [name, setName] = useState('');
+//   const [price, setPrice] = useState('');
+//   const [selectedDrinkId, setSelectedDrinkId] = useState(null);
+
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+//   const fetchData = async () => {
+//     try {
+//       const response = await axios.get('https://qrcodeweb-api.vercel.app/api/drinks'); // Thay đổi URL phù hợp
+//       setDrinks(response.data);
+//     } catch (error) {
+//       console.error('Error fetching data: ', error);
+//     }
+//   };
+
+//   const addDrink = async () => {
+//     try {
+//       const response = await axios.post('https://qrcodeweb-api.vercel.app/api/drinks', { name, price }); // Thay đổi URL phù hợp
+//       setDrinks([...drinks, response.data]);
+//       setName('');
+//       setPrice('');
+//     } catch (error) {
+//       console.error('Error adding drink: ', error);
+//     }
+//   };
+
+//   const updateDrink = async () => {
+//     try {
+//       if (!selectedDrinkId) {
+//         return;
+//       }
+
+//       await axios.patch(`https://qrcodeweb-api.vercel.app/api/drinks/${selectedDrinkId}`, { name, price }); // Thay đổi URL phù hợp
+//       fetchData();
+//       setName('');
+//       setPrice('');
+//       setSelectedDrinkId(null);
+//     } catch (error) {
+//       console.error('Error updating drink: ', error);
+//     }
+//   };
+
+//   const deleteDrink = async (id) => {
+//     try {
+//       await axios.delete(`https://qrcodeweb-api.vercel.app/api/drinks/${id}`); // Thay đổi URL phù hợp
+//       fetchData();
+//     } catch (error) {
+//       console.error('Error deleting drink: ', error);
+//     }
+//   };
+
+//   const selectDrink = (id, name, price) => {
+//     setSelectedDrinkId(id);
+//     setName(name);
+//     setPrice(price);
+//   };
+
+//   return (
+//     <div>
+//       <h2>Drinks</h2>
+//       <ul>
+//         {drinks.map((drink) => (
+//           <li key={drink._id}>
+//             {drink.name} - {drink.price}
+//             <button onClick={() => selectDrink(drink._id, drink.name, drink.price)}>Edit</button>
+//             <button onClick={() => deleteDrink(drink._id)}>Delete</button>
+//           </li>
+//         ))}
+//       </ul>
+//       <input
+//         type="text"
+//         placeholder="Name"
+//         value={name}
+//         onChange={(e) => setName(e.target.value)}
+//       />
+//       <input
+//         type="number"
+//         placeholder="Price"
+//         value={price}
+//         onChange={(e) => setPrice(e.target.value)}
+//       />
+//       {selectedDrinkId ? (
+//         <button onClick={updateDrink}>Update</button>
+//       ) : (
+//         <button onClick={addDrink}>Add Drink</button>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default DrinkComponent;
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function DrinkComponent() {
   const [drinks, setDrinks] = useState([]);
-  const [name, setName] = useState('');
-  const [price, setPrice] = useState('');
+  const [formData, setFormData] = useState({ name: '', price: '' });
   const [selectedDrinkId, setSelectedDrinkId] = useState(null);
 
   useEffect(() => {
@@ -159,10 +257,9 @@ function DrinkComponent() {
 
   const addDrink = async () => {
     try {
-      const response = await axios.post('https://qrcodeweb-api.vercel.app/api/drinks', { name, price }); // Thay đổi URL phù hợp
+      const response = await axios.post('https://qrcodeweb-api.vercel.app/api/drinks', formData); // Thay đổi URL phù hợp
       setDrinks([...drinks, response.data]);
-      setName('');
-      setPrice('');
+      setFormData({ name: '', price: '' });
     } catch (error) {
       console.error('Error adding drink: ', error);
     }
@@ -174,10 +271,9 @@ function DrinkComponent() {
         return;
       }
 
-      await axios.patch(`https://qrcodeweb-api.vercel.app/api/drinks/${selectedDrinkId}`, { name, price }); // Thay đổi URL phù hợp
+      await axios.patch(`https://qrcodeweb-api.vercel.app/api/drinks/${selectedDrinkId}`, formData); // Thay đổi URL phù hợp
       fetchData();
-      setName('');
-      setPrice('');
+      setFormData({ name: '', price: '' });
       setSelectedDrinkId(null);
     } catch (error) {
       console.error('Error updating drink: ', error);
@@ -195,8 +291,7 @@ function DrinkComponent() {
 
   const selectDrink = (id, name, price) => {
     setSelectedDrinkId(id);
-    setName(name);
-    setPrice(price);
+    setFormData({ name, price });
   };
 
   return (
@@ -211,26 +306,34 @@ function DrinkComponent() {
           </li>
         ))}
       </ul>
-      <input
-        type="text"
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        type="number"
-        placeholder="Price"
-        value={price}
-        onChange={(e) => setPrice(e.target.value)}
-      />
-      {selectedDrinkId ? (
-        <button onClick={updateDrink}>Update</button>
-      ) : (
-        <button onClick={addDrink}>Add Drink</button>
-      )}
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        if (selectedDrinkId) {
+          updateDrink();
+        } else {
+          addDrink();
+        }
+      }}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={formData.name}
+          onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+        />
+        <input
+          type="number"
+          placeholder="Price"
+          value={formData.price}
+          onChange={(e) => setFormData({ ...formData, price: e.target.value })}
+        />
+        {selectedDrinkId ? (
+          <button type="submit">Update</button>
+        ) : (
+          <button type="submit">Add Drink</button>
+        )}
+      </form>
     </div>
   );
 }
 
 export default DrinkComponent;
-
