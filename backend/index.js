faab0152a2256dfec03710d2b7a7a8086146c3e5
Long <nguyhonglong2002@gmail.com>
2023-12-08 17:07:31 +0700

    push

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(
     cors({
-        origin: 'http://localhost:3000',
+        origin: 'https://coffee.taxuatour.com',
         credentials: true,
     })
 );
